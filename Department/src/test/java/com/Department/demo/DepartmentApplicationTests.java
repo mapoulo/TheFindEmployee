dc03d69e96650ff5998c677f6ecfa4517e55cd65
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-12-05 14:38:40 +0200

    first commit

---
+++ Department/src/test/java/com/Department/demo/DepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Department.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
