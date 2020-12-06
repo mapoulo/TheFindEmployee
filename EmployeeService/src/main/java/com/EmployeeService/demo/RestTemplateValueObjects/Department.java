05462cd2dd0cd9b1d95d8e71795119b25e384d21
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-12-06 23:01:22 +0200

    DepartmentVO

---
+++ EmployeeService/src/main/java/com/EmployeeService/demo/RestTemplateValueObjects/Department.java
@@ -0,0 +1,14 @@
+package com.EmployeeService.demo.RestTemplateValueObjects;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Department {
+	
+    private int departmentId;
+	
+	private String departmentName;
+
+}
