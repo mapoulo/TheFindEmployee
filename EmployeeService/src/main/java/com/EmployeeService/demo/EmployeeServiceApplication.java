e497496d03003d520c9dcd6d3b10c4d7e3b9993d
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-12-06 23:06:31 +0200

    RestTemplate in the employee service application

--- EmployeeService/src/main/java/com/EmployeeService/demo/EmployeeServiceApplication.java
+++ EmployeeService/src/main/java/com/EmployeeService/demo/EmployeeServiceApplication.java
@@ -2,12 +2,19 @@ package com.EmployeeService.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new  RestTemplate();
 	}
 
 }
