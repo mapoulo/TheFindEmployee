1cddf241bbc7fe4d56eed4794a1e6ee1f312986a
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-12-06 21:37:08 +0200

    Employee Repository

---
+++ EmployeeService/src/main/java/com/EmployeeService/demo/Repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.EmployeeService.demo.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.EmployeeService.demo.Models.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
