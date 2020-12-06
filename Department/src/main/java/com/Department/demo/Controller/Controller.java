2b24371afee7b2204d1453493dda33c6f55d0b99
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-12-06 22:53:09 +0200

    Department Service

--- Department/src/main/java/com/Department/demo/Controller/Controller.java
+++ Department/src/main/java/com/Department/demo/Controller/Controller.java
@@ -1,15 +1,41 @@
 package com.Department.demo.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Department.demo.Models.Department;
+import com.Department.demo.Services.DepartmentService;
 
 @RestController
 @RequestMapping("/department")
 public class Controller {
 
-	@GetMapping("/")
-	public String getss() {
-		return "Heeeeeeee eeeee";
+    @Autowired
+	private DepartmentService service;
+	
+    
+    @PostMapping("/")
+	public void saveDepartment(@RequestBody Department department) {
+		service.saveDepartment(department);
+	}
+	
+	
+    @GetMapping("/{id}")
+	public Department getDepartmentById(@PathVariable int id) {
+		return service.getDepartmentById(id);
+	}
+	
+	
+    @GetMapping("/")
+	public List<Department> getAllDepartment(){
+		return service.getAllDepartment();
 	}
 }
